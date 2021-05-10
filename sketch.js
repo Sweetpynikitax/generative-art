7fc3a0a970dd7e04832e45dead0d3e15ec44cefb
Nikita <49714497+Sweetpynikitax@users.noreply.github.com>
2021-05-10 21:33:34 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 /* 👇 Start writing your p5.js code here */
 
-var points = [];
+var points = []
+var mult= 0.006
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background(30)
+  angleMode(DEGREES)
+  noiseDetail(1)
 
-  var density = 50;
+  var density = 80;
   var space = width/ density
 
   for(var x = 0; x < width; x  += space) {
     for(var y=0; y< height; y  += space){
-      var p = createVector(x,y)
+      var p = createVector(x+random(-10,10),y+random(-10,10))
       points.push(p)
     }
   }
@@ -20,14 +23,22 @@ function setup() {
 
 function draw() {
   noStroke()
-  fill(150)
+
 
   for (var i = 0; i< points.length; i++){
-    var angle = map(noise(points[i].x, points[i].y,),0,1,0,820)
+
+    var r= map(points[i].x, 0, width,50,255)
+    var g= map(points[i].y, 0, height,50,255)
+    var b= map(points[i].x, 0, width,50,255)
+
+    fill(r,g,b)
+
+
+    var angle = map(noise(points[i].x * mult, points[i].y * mult,),0,1,0,820)
 
     points[i].add(createVector(cos(angle),sin(angle)))
 
-
+    
     ellipse(points[i].x, points[i].y, 1)
 
   }
